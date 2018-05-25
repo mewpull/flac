c507848f507ee882bd1e5b257d1b9841bc5071f3
mewmew <rnd0x00@gmail.com>
2018-05-25 20:02:40 +0200

    meta: further safe-guard against error mismatch of io.EOF

--- meta/padding.go
+++ meta/padding.go
@@ -38,5 +38,5 @@ func (zr zeros) Read(p []byte) (n int, err error) {
 	}
 	// Note, not wrapping err in errors.WithStack as it will confuse io.Copy
 	// which is expecting io.EOF.
-	return n, err
+	return n, errors.Cause(err)
 }
